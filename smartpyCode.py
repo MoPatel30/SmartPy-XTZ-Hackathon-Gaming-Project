fcc9eecb7c268ce8b06888f44b0dfad5843f396f
Mohammed Patel <mopatel1214@gmail.com>
2020-06-24 16:25:58 -0500

    Update smartpyCode.py

--- smartpyCode.py
+++ smartpyCode.py
@@ -3,87 +3,45 @@ import random
 
 
 
-#scenario.h2("Player one transfers to Player two")
-#scenario += c1.transfer(f = alice.address, t = bob.address, amount = 4).run(sender = alice)
-#scenario.verify(c1.data.balances[alice.address].balance == 14)
-#plyrOne, plyrTwo
-
 class Game(sp.Contract):
     def __init__(self):
+
         self.init(
-            # playerOne = sp.address(addressOne)
-            # playerTwo = sp.address(addressTwo)
-            paused = False, 
-            #admin = sp.address(""),
-            one = sp.address('tz1WPF9AZU5SRZpUiKfPmdUmGMWkweSn6P3L'),
-            two = sp.address('tz1ZVQ4piGPKFiPKMejb7pvdPFdrHD3STbkH'),
-            totalSupply = 0,
-            bet = 0,
+            
             
             record_player_wins = {
-                "player_one" : sp.nat(0),
-                "player_two" : sp.nat(0)
+                "Player One" : sp.nat(0),
+                "Player Two" : sp.nat(0),
+                "Draw" : sp.nat(0)
+                
             }
+           
            
         )
         
-        
+    
+       
     @sp.entry_point
     def playerWins(self, player):
         self.data.record_player_wins[player] += 1
         
-#    @sp.entry_point
-#    def setAdministrator(self, params):
-#        sp.verify(sp.sender == self.data.administrator)
-#        self.data.administrator = params
-    
-    
-    @sp.entry_point
-    def transfer(self, amount, f, t): 
-        sp.verify(f.balance >= amount)
-        #self.addAddressIfNecessary(params.t)
-        sp.verify(t.balance >= amount)
-        f.balance -= amount
-        t.balance += amount
-        f.approvals[sp.sender] -= amount
-   
-   
-   # def addAddressIfNecessary(self, address):
-#        sp.if ~ self.data.balances.contains(address):
-#            self.data.balances[address] = sp.record(balance = 0, approvals = {})
-        
-        
-    @sp.entry_point
-    def betting(self, amount):
-        self.data.bet =  amount
-        
-        
-    
 
-
+        
+        
 def startGame():
-    
-    #alert("Player one input XTZ Address")
-    #addressOne = input()
-    
-    #alert("Player two input XTZ Address")
-    #addressTwo = input()
-
-    
       
-    alert("Enter betting amount in mxtz (1,000,000 mxtz = 1 xtz): ")
-    bettingAmount = input()
   
-    alert("Welcome to Betting Against the Odds!" + '\n' + "Guess seven random numbers between 0-100 and the closest for the majority number will win the pot." + '\n' + "Important: Be sure to only bet what you're willing to lose!" + '\n' + "Good Luck!")
-    alert("Player One enter seven guesses ranging from 0-100: ")
+    alert("Welcome to Betting Against the Odds!" + '\n' + "Guess seven random numbers between 0-100 and the closest for the majority of the numbers will win and be able to post a message to the blockchain." + '\n' + "Good Luck!")
+    
+    alert("Player one enter username: ")
+    player_one_name = input()
+    
+    alert("Player two enter username: ")
+    player_two_name = input()
+    alert(str(player_one_name) + " enter seven guesses ranging from 0-100: ")
     
     
-    
-#   alert("Agree upon a betting amount and enter here: ")
-#   bettingAmount = input()
-#   currentBet = float(bettingAmount)
-    
-#   sp.betting()
+
  
     guessOne = input()
     guessTwo = input()
@@ -97,7 +55,8 @@ def startGame():
     
     
     
-    alert("Player Two enter seven guesses ranging from 0-100: ")
+    alert(str(player_two_name) + " enter seven guesses ranging from 0-100: ")
+    
     TwoguessOne =  input()
     TwoguessTwo = input()
     TwoguessThree = input()
@@ -120,20 +79,22 @@ def startGame():
     randNums = [randNumOne, randNumTwo, randNumThree, randNumFour, randNumFive, randNumSix, randNumSeven]
     
     
-    alert("Player One Guesses: " + guessOne + ", " + guessTwo + ", " + guessThree +  ", " + guessFour +  ", " + guessFive +  ", " + guessSix +  ", " + guessSeven)
+    alert(player_one_name + " Guesses: " + guessOne + ", " + guessTwo + ", " + guessThree +  ", " + guessFour +  ", " + guessFive +  ", " + guessSix +  ", " + guessSeven)
     
-    alert("Player Two Guesses: " + TwoguessOne + ", " + TwoguessTwo + ", " + TwoguessThree +  ", " + TwoguessFour +  ", " + TwoguessFive +  ", " + TwoguessSix +  ", " + TwoguessSeven)
+    alert(player_two_name + " Guesses: " + TwoguessOne + ", " + TwoguessTwo + ", " + TwoguessThree +  ", " + TwoguessFour +  ", " + TwoguessFive +  ", " + TwoguessSix +  ", " + TwoguessSeven)
     
     alert("generated Numbers: "  + str(randNumOne) + ", " + str(randNumTwo) + ", " + str(randNumThree) +  ", " + str(randNumFour) +  ", " + str(randNumFive) +  ", " + str(randNumSix) +  ", " + str(randNumSeven))
-    
-    temp = determineWinner(guessesOne, guessesTwo, randNums, bettingAmount)
+
+    temp = determineWinner(guessesOne, guessesTwo, randNums,  player_one_name,  player_two_name)
     #temp.append(addressOne)
     #temp.append(addressTwo)
+    temp.append(player_one_name)
+    temp.append(player_two_name)
     return temp
     
 
 
-def determineWinner(plyr_one_guesses, plyr_two_guesses, rand_nums, amount):
+def determineWinner(plyr_one_guesses, plyr_two_guesses, rand_nums, player_one_name,  player_two_name):
     rounds = [0,0,0]
    
     
@@ -161,22 +122,18 @@ def determineWinner(plyr_one_guesses, plyr_two_guesses, rand_nums, amount):
        
             
     if rounds[0] > rounds[1]:
-        alert("Player One Wins!!!"  + "\n" + "Winning pot recieved: " + amount + " mxtz" + "\n" + str(rounds[0]) + " rounds won!" + "\n" + str(rounds[1]) + " rounds lossed :(" + "\n" + str(rounds[2]) + " rounds tied...")
+        alert(player_one_name + " Wins!!!"  + "\n" + str(rounds[0]) + " rounds won!" + "\n" + str(rounds[1]) + " rounds lossed :(" + "\n" + str(rounds[2]) + " rounds tied...")
         
     if rounds[0] == rounds[1]:
         alert("Game Tied! Prepare for next round!")
         startGame()
-    if rounds[0] < rounds[1]:
-        alert("Player Two Wins!!!"  + "\n" + "Winning pot recieved: " + amount + " mxtz" + "\n" + str(rounds[1]) + " rounds won!" + "\n" + str(rounds[0]) + " rounds lossed :(" + "\n" + str(rounds[2]) + " rounds tied...")
-    
-    
-    rounds.append(amount)
-    
-    alert("Want to start a new game? Enter (y/n)")
-    newGame = input()
-    if newGame == "y":
-        startGame()
         
+    if rounds[0] < rounds[1]:
+        alert(player_two_name +  " Wins!!!" + "\n" + str(rounds[1]) + " rounds won!" + "\n" + str(rounds[0]) + " rounds lossed :(" + "\n" + str(rounds[2]) + " rounds tied...")
+    
+  
+    
+
     return rounds
     
     
@@ -189,37 +146,25 @@ def test():
     scenario = sp.test_scenario()
     
 
-    #oneP = sp.test_account("Player One")
-    #twoP = sp.test_account("Player Two")
-    #scenario.h2("Accounts")
-    #scenario.show([oneP, twoP])
-        
-                
-    #test_game = Game(tester[4], tester[5])
     test_game = Game()
                 
     scenario += test_game
-    scenario += test_game.betting(int(tester[3]))
     
-#   test_game.setAdministrator(tester[4])
     
-#   test_game.data.one = tester[4]
-#   test_game.data.two = tester[5]
+    #test_game.data.record_player_wins[tester[3]] = test_game.data.record_player_wins.pop(test_game    .data.record_player_wins["player_one"])
+    #del test_game.data.record_player_wins["player_one"]
+    
+    #test_game.data.record_player_wins[tester[4]] = test_game.data.record_player_wins.pop(test_game.data.record_player_wins["player_two"])
+    #del test_game.data.record_player_wins["player_two"]
+    
+    for i in range(tester[2]):
+        scenario += test_game.playerWins("Draw")
     
       
     for i in range(tester[0]):
-        scenario += test_game.playerWins("player_one")
-        scenario.h2("Player two transfers to Player one")
-        scenario += test_game.transfer(amount = test_game.data.bet, f = test_game.data.two, t = test_game.data.one).run(valid = True, sender = test_game.data.two)
-        scenario.verify(test_game.data.balances[test_game.data.one.address].balance == 0)
-        
-       
+        scenario += test_game.playerWins("Player One")
         
         
     for j in range(tester[1]):
-        scenario += test_game.playerWins("player_two")
-        scenario.h2("Player one transfers to Player two")
-        scenario += test_game.transfer(amount = test_game.data.bet, f = test_game.data.one, t = test_game.data.two).run(valid = True, sender = test_game.data.one)
-        scenario.verify(test_game.data.balances[test_game.data.two.address].balance == 0)
-        
+        scenario += test_game.playerWins("Player Two")
         
